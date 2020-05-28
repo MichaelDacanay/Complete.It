e3098adc4c7dd536b7114e41cd69aa03e8ce2529
mzdacana <mzdacana@ncsu.edu>
2020-05-28 16:31:33 -0400

    Add login component

--- flask-crm/frontend/src/app/app.module.ts
+++ flask-crm/frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
+import { LoginComponent } from './components/layout/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
